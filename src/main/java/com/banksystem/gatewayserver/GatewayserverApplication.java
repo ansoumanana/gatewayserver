99fc52b875abd21cc85bc35ceb0666423e9fc3e4
acisse <ansoumana.cisse@photobox.com>
2022-11-19 19:11:22 +0100

    banksystem : gateway server

---
+++ src/main/java/com/banksystem/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,45 @@
+package com.banksystem.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder ){
+
+		return builder.routes()
+				.route(p -> p
+						.path("/banksystem/account/**")
+						.filters(f -> f
+								.rewritePath("/banksystem/account/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://ACCOUNT")).
+				route(p -> p
+						.path("/banksystem/loans/**")
+						.filters(f -> f
+								.rewritePath("/banksystem/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://LOANS")).
+				route(p -> p
+						.path("/banksystem/cards/**")
+						.filters(f -> f
+								.rewritePath("/banksystem/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://CARDS")).build();
+
+	}
+
+}
